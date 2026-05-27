dd3c3a469defcc97876881e32e977b84f545615f
LaurenceFluciano <laurencefluciano@gmail.com>
2026-05-26 22:05:31 -0300

    feat(task): add remove and update functions to change task state.

--- to-do/src/lib/task.ts
+++ to-do/src/lib/task.ts
@@ -1,4 +1,3 @@
-
 import type { TaskAction, TaskItem } from "../types/Task";
 
 export function taskReducer(state: TaskItem[], action: TaskAction) {
@@ -7,21 +6,26 @@ export function taskReducer(state: TaskItem[], action: TaskAction) {
             return createTask(state)
         
         case "REMOVE":
-
-            break;
+            return removeTask(state, action.id)
         
         case "UPDATE":
-
-            break;
+            return updateTask(state, action.payload, action.id)
 
         default:
             return state;
     }
 }
 
-const createTask = (prev: TaskItem[]) => {
+const createTask = (state: TaskItem[]) => {
+
+    const cleanedState = state.filter(task => {
+        const isEmpty = task.name.trim() === '' && task.description.trim() === '';
+        return !isEmpty;
+    });
+
+   
     return [
-        ...prev,
+        ...cleanedState,
         {
         id: crypto.randomUUID().toString(),
         name: '',
@@ -30,3 +34,24 @@ const createTask = (prev: TaskItem[]) => {
         } as TaskItem
     ]
 }
+
+const removeTask = (tasks: TaskItem[], id: string) => {
+    
+    return tasks.filter(task => {
+        if (task.id != id) {
+            return task
+        }
+        return;
+    })
+}
+
+const updateTask = (tasks: TaskItem[], payload: Partial<TaskItem>, id: string) => {
+    
+    return tasks.map(task => {
+            if (task.id == id) {
+                return { ...task, ...payload };
+            }
+            return task
+        })
+}
+
